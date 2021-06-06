27abc6cbc175667bffbaa1fc77da32049e150947
pedrocarvalho77 <up201805068@fc.up.pt>
2021-06-06 12:34:01 +0100

    clf a correr quer com holdout quer com cv

--- src/AI.py
+++ src/AI.py
@@ -6,13 +6,29 @@ from sklearn import tree
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_validate
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import ConfusionMatrixDisplay
+
+def remove_columns(speedDating):
+    #condição para verificar se existe missing values em mais de 70% das entradas para cada coluna
+    total_rows=speedDating.shape[0]
+    print("Number of rows =",total_rows)
+    print("\nTotal Nulls per column\n" ,speedDating.isnull().sum())
+    print("\nCondition to verify if there more then 70% of nulls per column\n" ,speedDating.isnull().sum() >0.7*total_rows)
+
+    #como em nenhuma situaçao tal se verifica nao eliminamos colunas
+
+def remove_row(speedDating):
+    # Eliminar linhas
+    row_speedDating = speedDating.dropna()
+    row_speedDating.info()
+
+    #remove cerca de 18% das linhas o que tendo em conta que sao apenas 8378
+    #creio que sao demasiadas para este problema, assims sendo nao serao removidas linhas
 
 def calc_mean_median_mode(my_list,speedDating):
     for i in my_list:
@@ -35,7 +51,6 @@ def holdout(X,y,clf,size):
     print_holdout_scores(y_test,y_predict)
 
 def cross_validation(X,y,clf,size):
-    #scoring = ['accuracy','precision','recall','f1']
     kf = KFold(n_splits=size,random_state=42, shuffle=True)
     id = 1
     total_tn = 0
@@ -52,9 +67,7 @@ def cross_validation(X,y,clf,size):
         total_fp += fp
         total_fn += fn
         total_tp += tp
-        #scores = cross_validate(clf,X,y,cv=size,scoring=scoring)
         id += 1
-        #print_all_scores(scoring,scores,False)
     print_cv_scores(total_tn,total_fp,total_fn,total_tp)
 
 def GNB(X,y,size):
@@ -95,39 +108,15 @@ def print_cv_scores(tn,fp,fn,tp):
     print('Recall Score: %.3f' % recall)
     print('F1 Score: %.3f' % f1)
 
-def print_all_scores(scoring,scores,mean):
-    for i in scoring:
-        if mean == True:
-            print(i,"score: %.3f" % scores['test_'+i].mean())
-        else:
-            print(i,scores['test_'+i].round(3))
-
-def export_tree(depth,clf):
+def export_tree(type,depth,clf):
     features_names= ['id','partner','age','age_o','goal','date','go_out','int_corr','length','met','like','prob']
     target_names = ['0','1']
     dot = tree.export_graphviz(clf,max_depth=depth,out_file=None,feature_names=features_names,class_names=target_names,filled=True,rounded=True)
     graph = pydotplus.graph_from_dot_data(dot)
     Image(graph.create_png())
     file_path="/home/pedro/Faculdade/3ano/2sem/IA/trabalho2/AI-Machine-Learning/pictures/"
-    graph_name = file_path+"tree_depth_"+str(depth)+".png"
+    graph_name = file_path+type+"tree_depth_"+str(depth)+".png"
     graph.write_png(graph_name)
-
-def remove_columns(speedDating):
-    #condição para verificar se existe missing values em mais de 70% das entradas para cada coluna
-    total_rows=speedDating.shape[0]
-    print("Number of rows =",total_rows)
-    print("\nTotal Nulls per column\n" ,speedDating.isnull().sum())
-    print("\nCondition to verify if there more then 70% of nulls per column\n" ,speedDating.isnull().sum() >0.7*total_rows)
-
-    #como em nenhuma situaçao tal se verifica nao eliminamos colunas
-
-def remove_row(speedDating):
-    # Eliminar linhas
-    row_speedDating = speedDating.dropna()
-    row_speedDating.info()
-
-    #remove cerca de 18% das linhas o que tendo em conta que sao apenas 8378
-    #creio que sao demasiadas para este problema, assims sendo nao serao removidas linhas
 
 def main():
     speedDating = pd.read_csv("~/Faculdade/3ano/2sem/IA/trabalho2/AI-Machine-Learning/speedDating_trab.csv")
@@ -142,19 +131,19 @@ def main():
     X = speedDating[["id","partner","age","age_o","goal","date","go_out","int_corr","length","met","like","prob"]]
     y = speedDating["match"]
 
-    print(len(speedDating[speedDating.match==1]))
-    print(len(speedDating[speedDating.match==0]))
+    #print(len(speedDating[speedDating.match==1]))
+    #print(len(speedDating[speedDating.match==0]))
     clf = tree.DecisionTreeClassifier()
+    print("Holdout")
+    holdout(X,y,clf,0.3)
+    export_tree("holdout",6,clf)
 
-    x = input("Holdout or Cross Validation (h or cv)? ")
-    if x == "h":
-        holdout(X,y,clf,0.3)
-    elif x=="cv":
-        cross_validation(X,y,clf,5)
+    clf = tree.DecisionTreeClassifier()
+    print("\nCross Validation")
+    cross_validation(X,y,clf,5)
+    export_tree("cv",6,clf)
 
-    GNB(X,y,0.5)
-
-    export_tree(6,clf)
+    #GNB(X,y,0.5)
 
 if __name__ == "__main__":
     main()
